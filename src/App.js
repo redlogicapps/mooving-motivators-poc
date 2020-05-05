7caa144ed6b8cb0537b94871d2e911ee47d6eda6
redlogicapps <blejdea.ovidiu@gmail.com>
2020-05-05 22:04:25 +0200

    cards now move in the middle row, but they don't persist

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Grid from "@material-ui/core/Grid";
 import Card from "./components/card/card.component";
-import acceptance from "./images/acceptance.png";
+import acceptanceImage from "./images/acceptance.png";
 import curiosity from "./images/curiosity.png";
 import goal from "./images/goal.png";
 import honor from "./images/honor.png";
@@ -13,58 +13,109 @@ import power from "./images/power.png";
 import relatedness from "./images/relatedness.png";
 import status from "./images/status.png";
 import Paper from "@material-ui/core/Paper";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      images: [
-        { src: acceptance, key: 1 },
-        { src: curiosity, key: 2 },
-        { src: freedom, key: 3 },
-        { src: goal, key: 4 },
-        { src: honor, key: 5 },
-        { src: mastery, key: 6 },
-        { src: order, key: 7 },
-        { src: power, key: 8 },
-        { src: relatedness, key: 9 },
-        { src: status, key: 10 },
+      cards: [
+        { id: "acceptance", src: acceptanceImage, key: 10 },
+        { id: "curiosity", src: curiosity, key: 11 },
+        { id: "freedom", src: freedom, key: 12 },
+        { id: "goal", src: goal, key: 13 },
+        { id: "honor", src: honor, key: 14 },
+        { id: "mastery", src: mastery, key: 15 },
+        { id: "order", src: order, key: 16 },
+        { id: "power", src: power, key: 17 },
+        { id: "relatedness", src: relatedness, key: 18 },
+        { id: "status", src: status, key: 19 },
       ],
     };
   }
 
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
+
+    if (destination.droppableId === source.droppableId &&
+      destination.index === source.index) {
+        return;
+      }
+  };
+
   render() {
     return (
-      <div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
         <h1 className="title">Moving Motivators v0.0.1</h1>
         <Grid item xs={12}>
           <Grid container justify="center" spacing={2}>
-            {this.state.images.map((value) => (
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+              <Grid key={value} item>
+                <Droppable
+                  droppableId={value.toString()}
+                  direction="horizontal"
+                >
+                  {(provided) => (
+                    <Paper
+                      className="paper"
+                      elevation={3}
+                      innerRef={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      {provided.placeholder}
+                    </Paper>
+                  )}
+                </Droppable>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            {this.state.cards.map((value, index) => (
               <Grid key={value.key} item>
-              <Paper className="paper" elevation={3}><Card src={value.src} /></Paper>
+                <Droppable droppableId={value.id} direction="horizontal">
+                  {(provided) => (
+                    <Paper
+                      className="paper-center"
+                      elevation={3}
+                      innerRef={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <Card src={value.src} id={value.id} index={index} />
+                      {provided.placeholder}
+                    </Paper>
+                  )}
+                </Droppable>
               </Grid>
             ))}
           </Grid>
         </Grid>
         <Grid item xs={12}>
           <Grid container justify="center" spacing={2}>
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+            {[20, 21, 22, 23, 24, 25, 26, 27, 28, 29].map((value) => (
               <Grid key={value} item>
-                <Paper className="paper-center" elevation={3} />
+                <Droppable
+                  droppableId={value.toString()}
+                  direction="horizontal"
+                >
+                  {(provided) => (
+                    <Paper
+                      className="paper"
+                      elevation={3}
+                      innerRef={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      {provided.placeholder}
+                    </Paper>
+                  )}
+                </Droppable>
               </Grid>
             ))}
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={2}>
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
-              <Grid key={value} item>
-                <Paper className="paper" elevation={3} />
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </div>
+      </DragDropContext>
     );
   }
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,23 @@
-// @flow 
-import * as React from 'react';
+// @flow
+import * as React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 export const Card = (props) => {
-    return (
-        <img alt="motivator" src={props.src} width="150px" sizes="150px"></img>
-    );
+  return (
+    <Draggable key={props.id} draggableId={props.id} index={props.index}>
+      {(provided) => (
+        <img
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          alt="motivator"
+          src={props.src}
+          width="150px"
+          sizes="150px"
+        ></img>
+      )}
+    </Draggable>
+  );
 };
 
 export default Card;
