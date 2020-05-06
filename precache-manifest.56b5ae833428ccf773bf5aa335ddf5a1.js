4e52145f39898edc4adf6e6551fd580b4e3d9dbc
redlogicapps <blejdea.ovidiu@gmail.com>
2020-05-06 10:57:29 +0200

    Updates

---
+++ precache-manifest.56b5ae833428ccf773bf5aa335ddf5a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78722ebb20f8af679586eae0dec97d8b",
+    "url": "/mooving-motivators-poc/index.html"
+  },
+  {
+    "revision": "aa15793c8499e1ec47a6",
+    "url": "/mooving-motivators-poc/static/css/main.1d737570.chunk.css"
+  },
+  {
+    "revision": "266d3ff337bf7a6b0e6d",
+    "url": "/mooving-motivators-poc/static/js/2.df179cb7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mooving-motivators-poc/static/js/2.df179cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa15793c8499e1ec47a6",
+    "url": "/mooving-motivators-poc/static/js/main.675986eb.chunk.js"
+  },
+  {
+    "revision": "32223bf8bd380624a620",
+    "url": "/mooving-motivators-poc/static/js/runtime-main.4a986d3b.js"
+  },
+  {
+    "revision": "f9297b4ad84cd714e0853620de26e7b8",
+    "url": "/mooving-motivators-poc/static/media/acceptance.f9297b4a.png"
+  },
+  {
+    "revision": "faccd34323abd723d5c5fcc8976cba9c",
+    "url": "/mooving-motivators-poc/static/media/curiosity.faccd343.png"
+  },
+  {
+    "revision": "801795ab110f96921a529b1fa5e0a2ce",
+    "url": "/mooving-motivators-poc/static/media/freedom.801795ab.png"
+  },
+  {
+    "revision": "2dffd3e01baa6b3eef42e503e8bf227c",
+    "url": "/mooving-motivators-poc/static/media/goal.2dffd3e0.png"
+  },
+  {
+    "revision": "c5e2ba5a32e1420b7d0cb8a9ede161a1",
+    "url": "/mooving-motivators-poc/static/media/mastery.c5e2ba5a.png"
+  },
+  {
+    "revision": "3feceb4485add52335157dfa3cf4baa7",
+    "url": "/mooving-motivators-poc/static/media/power.3feceb44.png"
+  },
+  {
+    "revision": "915cedde65e2c912e5472107b482b295",
+    "url": "/mooving-motivators-poc/static/media/relatedness.915cedde.png"
+  }
+]);
